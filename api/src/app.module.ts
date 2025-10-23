6f7960ef0e029de4ec20509d343a9641746ff6fc
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-10-23 15:39:58 +0200

    config init

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DubizzleModule } from './dubizzle/dubizzle.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DubizzleModule],
+  imports: [DubizzleModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/dubizzle/dubizzle.service.ts
+++ api/src/dubizzle/dubizzle.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class DubizzleService {}
+export class DubizzleService {
+  constructor(private readonly httpService: HttpService) {
+     Logger.log({cookie:process.env.reese84}) 
+  }
+  async getTokens() {   
+    console.log({cookie:process.env.test}) 
+  }
+}
