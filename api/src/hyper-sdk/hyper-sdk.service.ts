a57e4932624b810d042add65da393b9221a92184
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-11-03 14:49:59 +0100

    logger

--- api/src/hyper-sdk/hyper-sdk.service.ts
+++ api/src/hyper-sdk/hyper-sdk.service.ts
@@ -1,18 +1,23 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Session } from 'hyper-sdk-js';
 @Injectable()
 export class HyperSdkService implements OnModuleInit {
+  private readonly logger = new Logger(HyperSdkService.name);
   constructor(
     private session: Session,
     private configService: ConfigService,
   ) {}
   onModuleInit() {
     const apiKey = this.configService.get<string>('HYPER_SDK_API_KEY');
-
-    if(!apiKey){
-        throw 
+    this.logger.log({ apiKey });
+    if (!apiKey) {
+      const errorInfo = {
+        message: 'Conig error. HYPER_SDK_API_KEY is can not be reached.',
+        date: new Date(),
+      };
+      throw new Error(JSON.stringify(errorInfo));
     }
-    this.session = new Session(apiKey!);
+    // this.session = new Session(apiKey!);
   }
 }
