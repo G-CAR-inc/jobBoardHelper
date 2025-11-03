64e3c2f36f558ac2db4e6e0fc14347956b434f95
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-11-03 13:56:26 +0100

    .env.example init

--- api/src/hyper-sdk/hyper-sdk.service.ts
+++ api/src/hyper-sdk/hyper-sdk.service.ts
@@ -1,10 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Session } from 'hyper-sdk-js';
 @Injectable()
 export class HyperSdkService implements OnModuleInit {
-  constructor(private session: Session) {}
+  constructor(
+    private session: Session,
+    private configService: ConfigService,
+  ) {}
   onModuleInit() {
-    
-    this.session = new Session('your-api-key');
+    const apiKey = this.configService.get<string>('HYPER_SDK_API_KEY');
+
+    if(!apiKey){
+        throw 
+    }
+    this.session = new Session(apiKey!);
   }
 }
