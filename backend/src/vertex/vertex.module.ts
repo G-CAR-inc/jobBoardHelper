0742588c5ca34ae9843f6c2461808214cf29ab79
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-12-29 15:12:44 +0100

    http module import

--- backend/src/vertex/vertex.module.ts
+++ backend/src/vertex/vertex.module.ts
@@ -3,8 +3,10 @@ import { VertexService } from './vertex.service';
 import { VertexController } from './vertex.controller';
 import { VertexRepository } from './repositories/vertex-repo.repository';
 import { SchedulerService } from './scheduler/vertex-scheduler.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [VertexController],
   providers: [VertexService, VertexRepository, SchedulerService],
 })
